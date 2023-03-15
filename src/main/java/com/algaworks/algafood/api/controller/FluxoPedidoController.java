273927993814e764decb1231e224e21d18f71bea
andre-alcas <alcas.andre@gmail.com>
2023-03-14 23:04:14 -0300

    status de criado para cancelado

---
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.service.FluxoPedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos/{pedidoId}")
+public class FluxoPedidoController {
+  
+    @Autowired
+    private FluxoPedidoService fluxoPedido;
+    
+    @PutMapping("/confirmacao")
+    public void confirmar(@PathVariable Long pedidoId) {
+    	fluxoPedido.confirmar(pedidoId);
+    }
+    
+   
+}  
