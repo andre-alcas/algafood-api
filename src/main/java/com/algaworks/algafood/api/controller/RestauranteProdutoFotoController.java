4e7ca7985f06b11b642b057cf559d4e1a28a81d8
andre-alcas <alcas.andre@gmail.com>
2023-03-27 20:50:22 -0300

    validacao tamanho de arquivo

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,16 +19,16 @@ public class RestauranteProdutoFotoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void atualizarFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
 		
 		var nomeArquivo = UUID.randomUUID().toString() 
 				+ "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 		
 		var arquivoFoto = Path.of("/Users/alcas/catalogo", nomeArquivo);
 		
-		System.out.println(fotoProdutoInput.getDescricao());
-		System.out.println(arquivoFoto);
-		System.out.println(fotoProdutoInput.getArquivo().getContentType());
+		//System.out.println(fotoProdutoInput.getDescricao());
+		//System.out.println(arquivoFoto);
+		//System.out.println(fotoProdutoInput.getArquivo().getContentType());
 		
 		try {
 			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
