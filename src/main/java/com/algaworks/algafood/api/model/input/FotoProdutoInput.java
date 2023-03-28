ee5650ef26ceb987d7a40229658cc54c5df921f0
andre-alcas <alcas.andre@gmail.com>
2023-03-27 21:27:16 -0300

    validacao tipo de arquivo

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -2,11 +2,10 @@ package com.algaworks.algafood.api.model.input;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
 import org.springframework.web.multipart.MultipartFile;
-
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
-
+import org.springframework.http.MediaType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +15,7 @@ public class FotoProdutoInput {
 
 	@NotNull
 	@FileSize(max= "50KB")
+	@FileContentType(allowed= {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 	
 	@NotBlank
---
+++ src/main/java/com/algaworks/algafood/core/validation/FileContentTypeValidator.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.core.validation;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileContentTypeValidator implements ConstraintValidator<FileContentType, MultipartFile> {
+
+	private List<String> tipo; 
+	
+	@Override
+	public void initialize(FileContentType constraintAnnotation) {
+		this.tipo =  Arrays.asList(constraintAnnotation.allowed());
+	}
+
+	@Override
+	public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
+		return value == null || this.tipo.contains(value.getContentType());
+	}//value.getSize() é retornado em Bytes entao tem que comparar com Bytes do maxSize;
+
+}
