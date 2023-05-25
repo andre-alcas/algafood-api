e48a8ba71ad6ed65ffe077fb494720e301985fef
andre-alcas <alcas.andre@gmail.com>
2023-05-24 21:36:42 -0300

    add api v2 cont

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.algafood.api.v1.model.EnderecoModel;
 import com.algaworks.algafood.api.v1.model.input.ItemPedidoInput;
+import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.ItemPedido;
 
@@ -16,6 +18,9 @@ public class ModelMapperConfig {
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
 
+		modelMapper.createTypeMap(CidadeInputV2.class, Cidade.class)
+		.addMappings(mapper -> mapper.skip(Cidade::setId));
+		
 //		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
 //			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
 
