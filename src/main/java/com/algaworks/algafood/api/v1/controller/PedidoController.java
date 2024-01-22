a1e685b70237ee258a7a8734bbb670b267f844ad
andre-alcas <alcas.andre@gmail.com>
2024-01-22 20:24:40 -0300

    pedido pode consultar

--- src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
@@ -68,6 +68,7 @@ public class PedidoController implements PedidoControllerOpenApi {
     @Autowired
     private AlgaSecurity algaSecurity;
 
+    @CheckSecurity.Pedidos.PodePesquisar
     @Override
 	@GetMapping
     public PagedModel<PedidoResumoModel> pesquisar(PedidoFilter filtro,@PageableDefault(size=10) Pageable pageable) {
