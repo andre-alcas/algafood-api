81d3772b5fce64c653a95abb1cf40a5146d9bc76
andre-alcas <alcas.andre@gmail.com>
2024-01-30 20:14:14 -0300

    estatisticasController permissoes

--- src/main/java/com/algaworks/algafood/api/v1/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/EstatisticasController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.api.v1.AlgaLinks;
 import com.algaworks.algafood.api.v1.model.dto.VendaDiaria;
 import com.algaworks.algafood.api.v1.openapi.controller.EstatisticasControllerOpenApi;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 import com.algaworks.algafood.domain.service.VendaReportService;
@@ -33,6 +34,7 @@ public class EstatisticasController implements EstatisticasControllerOpenApi {
 	@Autowired
 	private AlgaLinks algaLinks;
 	
+	@CheckSecurity.Estatisticas.PodeConsultar
 	@Override
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public EstatisticasModel estatisticas() {
@@ -43,12 +45,14 @@ public class EstatisticasController implements EstatisticasControllerOpenApi {
 	    return estatisticasModel;
 	}
 
+	@CheckSecurity.Estatisticas.PodeConsultar
 	@Override
 	@GetMapping(path = "/vendas-diarias",produces= MediaType.APPLICATION_JSON_VALUE)
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, @RequestParam(required=false, defaultValue= "+00:00") String timeOffset) {
 		return vendaQueryService.consultarVendasDiarias(filtro,timeOffset);
 	}
 
+	@CheckSecurity.Estatisticas.PodeConsultar
 	@Override
 	@GetMapping(path = "/vendas-diarias",produces= MediaType.APPLICATION_PDF_VALUE)
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro, @RequestParam(required=false, defaultValue= "+00:00") String timeOffset) {
